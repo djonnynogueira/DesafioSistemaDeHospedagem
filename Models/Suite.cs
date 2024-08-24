8c90461a99db0523a9b90403d0232d9535d31a9e
Djonny Nogueira <djonnynogueira@gmail.com>
2024-08-24 15:28:16 -0300

    Criação do projeto - dotnet new console. Definição das classes Pessoa e Suite

---
+++ Models/Suite.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioSistemaDeHospedagem.Models
+{
+    public class Suite
+    {
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+
+    }
+}
